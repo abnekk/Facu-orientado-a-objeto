27e5293cae093ab18abc1958e7690fa0d31fd766
abnekk <37461314+abnekk@users.noreply.github.com>
2018-05-01 16:09:38 -0300

    Add files via upload

---
+++ Veiculo/P1.cpp
@@ -0,0 +1,16 @@
+#include "Veiculos.h"
+
+int main(int argc, char *argv[])
+{
+	
+	vector <Carro*> cr;
+	vector <Caminhao*> cm;
+	vector <Moto*> mo;
+	int i = 2;
+	
+	while(i>=1){
+		
+	};
+	system("pause");
+	return 0;
+}
