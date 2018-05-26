31d3ef026297c7590689626a42c4435a73f213bb
abnekk <37461314+abnekk@users.noreply.github.com>
2018-05-25 22:33:47 -0300

    Update Bandido.h

--- Projeto-Jogo de tiro/Bandido.h
+++ Projeto-Jogo de tiro/Bandido.h
@@ -10,9 +10,8 @@ using namespace std;
 class Bandido : public Jogador{// sub-classe Bandido
 	private:
 		double Vida = 100.00;
-		int Balas = 20;
 	public:
-		void Dano(){
+		int Dano(){
 			int r;
 			srand (time(NULL));
 			r = rand() % 4 + 1;
@@ -20,27 +19,26 @@ class Bandido : public Jogador{// sub-classe Bandido
 				int cabeca;
 				cabeca = rand() % 20 + 50;
 				cout << "Acertou a cabeca: " << cabeca << " de dano" << endl;
-				Vida = Vida-cabeca;
-				cout << "Vida atual: " << Vida << endl;
+				return cabeça;
 				
 			}
 			if(r==2){
 				int Torax;
 				Torax = rand() % 35 + 1;
 				cout << "Acertou o torax: " << Torax << " de dano" << endl;
-				Vida = Vida-Torax;
-				cout << "Vida atual: " << Vida << endl;
+				return Torax;
+				
 			}
 			if(r==3){
 				int Membros;
 				Membros = rand() % 25 + 1;
 				cout << "Acertou um membro: " << Membros << " de dano" << endl;
-				Vida = Vida-Membros;
-				cout << "Vida atual: " << Vida << endl;
+				return Membros;
 			}
 			if(r==4){
 				cout << "Erro" << endl;
 				cout << "Vida atual: " << Vida << endl;
+				return 0;
 			}
 		}	
 	void setVida(int Vida_Bandido){
@@ -48,12 +46,6 @@ class Bandido : public Jogador{// sub-classe Bandido
 	}
 	int getVida(){
 		return Vida;
-	}
-	void setBalas(int Balas_Bandido){
-		Balas=Balas_Bandido;
-	}
-	int getBalas(){
-		return Balas;
 	}
 	
 };
