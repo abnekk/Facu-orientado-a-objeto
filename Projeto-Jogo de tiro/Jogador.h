f8d02529402de9aec9b45e630b9730fef05f4a77
abnekk <37461314+abnekk@users.noreply.github.com>
2018-05-25 22:33:11 -0300

    Update Jogador.h

--- Projeto-Jogo de tiro/Jogador.h
+++ Projeto-Jogo de tiro/Jogador.h
@@ -1,7 +1,5 @@
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <vector>
+#include "Policia.h"
+#include "Bandido.h"
 
 using namespace std;
 
@@ -16,7 +14,7 @@ class Jogador{//Class do usuario
 			cout << "Atacou" << endl;
 			
 		}
-		Jogador(string Nick_Jogador, int Senha_Jogador){
+		Jogador(string Nick_Jogador, string Senha_Jogador){
 			Nick=Nick_Jogador;
 			Senha=Senha_Jogador;
 		}
@@ -32,6 +30,44 @@ class Jogador{//Class do usuario
 		int getPlacar(){
 			return Placar;
 		}
+		int combate(int Player){
+			switch(Player == 1){
+				while(1){
+					string i;
+                    cout << "Coloque qualquer coisa para atacar"<< endl;
+                    cin << i;
+					int D = Bandido.Dano();
+					Policia.setVida() = Policia.setVida() - D;
+					cout << "Vida atual do policial: " << Policia.getVida() << endl;
+					if(Policia.getVida()<= 0){
+						return 1;
+					}
+					cout << "Ataque inimigo"<< endl;
+					int D = Policia.Dano();
+					Bandido.setVida() = Bandido.setVida() - D;
+					cout << "Vida atual do bandido: " << Bandido.getVida() << endl;
+					if(Bandido.getVida()<= 0){
+						return 2;
+					}
+				
+			switch(Player = 2){
+				while(1){
+					string i;
+                    cout << "Coloque qualquer coisa para atacar"<< endl;
+                    cin << i;
+					int D = Policia.Dano();
+					Bandido.setVida() = Bandido.setVida() - D;
+					cout << "Vida atual do bandido: " << Bandido.getVida() << endl;
+					switch(Bandido.getVida()<= 0){
+						return 1;
+					}
+					cout << "Ataque inimigo"<< endl;
+					int D = Bandido.Dano();
+					Policia.setVida() = Policia.setVida() - D;
+					cout << "Vida atual do policia: " << Policia.getVida() << endl;
+					switch(Policial.getVida()<= 0){
+						return 2;
+					}
 		virtual ~Jogador(){};
 		
 };
