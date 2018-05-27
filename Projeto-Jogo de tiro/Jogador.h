9193c66d0254ed7a66744d0eeaa9ea49b2e4074b
abnekk <37461314+abnekk@users.noreply.github.com>
2018-05-27 18:02:34 -0300

    Update Jogador.h

--- Projeto-Jogo de tiro/Jogador.h
+++ Projeto-Jogo de tiro/Jogador.h
@@ -1,6 +1,8 @@
-#include "Policia.h"
-#include "Bandido.h"
-
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <time.h>
 using namespace std;
 
 class Jogador{//Class do usuario
@@ -11,7 +13,7 @@ class Jogador{//Class do usuario
 
 	public:
 		virtual void Dano(){
-			cout << "Atacou" << endl;
+			cout << "Atacou errado" << endl;
 			
 		}
 		Jogador(string Nick_Jogador, string Senha_Jogador){
@@ -30,47 +32,127 @@ class Jogador{//Class do usuario
 		int getPlacar(){
 			return Placar;
 		}
-		int combate(int Player){
-			switch(Player == 1){
-				while(1){
-					string i;
-                    cout << "Coloque qualquer coisa para atacar"<< endl;
-                    cin << i;
-					int D = Bandido.Dano();
-					Policia.setVida() = Policia.setVida() - D;
-					cout << "Vida atual do policial: " << Policia.getVida() << endl;
-					if(Policia.getVida()<= 0){
-						return 1;
-					}
-					cout << "Ataque inimigo"<< endl;
-					int D = Policia.Dano();
-					Bandido.setVida() = Bandido.setVida() - D;
-					cout << "Vida atual do bandido: " << Bandido.getVida() << endl;
-					if(Bandido.getVida()<= 0){
-						return 2;
-					}
-				
-			switch(Player = 2){
-				while(1){
-					string i;
-                    cout << "Coloque qualquer coisa para atacar"<< endl;
-                    cin << i;
-					int D = Policia.Dano();
-					Bandido.setVida() = Bandido.setVida() - D;
-					cout << "Vida atual do bandido: " << Bandido.getVida() << endl;
-					switch(Bandido.getVida()<= 0){
-						return 1;
-					}
-					cout << "Ataque inimigo"<< endl;
-					int D = Bandido.Dano();
-					Policia.setVida() = Policia.setVida() - D;
-					cout << "Vida atual do policia: " << Policia.getVida() << endl;
-					switch(Policial.getVida()<= 0){
-						return 2;
-					}
+		
+	
 		virtual ~Jogador(){};
 		
 };
 
-
-
+class Policia : public Jogador{ // sub-classe Policia
+	private:
+		int Vida;// Sera a vida do Bandido
+	public:
+		void vidona(int Vidinha){
+			Vida=Vidinha;
+		}
+		void Dano(){//Dano que o Policial da no Bandido.
+			int r;
+			srand (time(NULL));
+			r = rand() % 4 + 1;
+			if(r==1){
+				int cabeca; 
+				cabeca = rand() % 20 + 60;
+				cout << "Acertou a cabeca: " << cabeca << " de dano" << endl;
+				Vida = Vida-cabeca;
+				cout << "Vida atual: " << Vida << endl;
+				
+			}
+			if(r==2){
+				int Torax;
+				Torax = rand() % 25 + 1;
+				cout << "Acertou o torax: " << Torax << " de dano" << endl;
+				Vida = Vida-Torax;
+				cout << "Vida atual: " << Vida << endl;
+			}
+			if(r==3){
+				int Membros;
+				Membros = rand() % 15 + 1;
+				cout << "Acertou um membro: " << Membros << " de dano" << endl;
+				Vida = Vida-Membros;
+				cout << "Vida atual: " << Vida << endl;
+			}
+			if(r==4){
+				cout << "Erro" << endl;
+				cout << "Vida atual: " << Vida << endl; 
+			}
+		}
+		int combate(){//O ataque do Policial
+					string i;
+                    cout << "Coloque qualquer coisa para atacar"<< endl;
+                    cin >> i;
+					Dano();
+					cout << "Vida atual do Bandido: " << Vida << endl;
+					if(Vida<= 0){
+						return 1;
+					}
+			}
+		
+		void setVida(int Vida_Bandido){//Aqui sra guardado a vida do Bandido
+			Vida=Vida_Bandido;
+		}
+		int getVida(){
+			return Vida;
+		}
+};
+ 
+class Bandido : public Jogador{// sub-classe Bandido
+	private:
+		int Vida;// Sera a vida do Policial
+	public:
+		void vidona(int Vidinha){
+			Vida=Vidinha;
+		}
+		void Dano(){//Dano que o Bandido da no Policial.
+			int r;
+			srand (time(NULL));
+			r = rand() % 4 + 1;
+			if(r==1){
+				int cabeca;
+				cabeca = rand() % 20 + 60;
+				cout << "Acertou a cabeca: " << cabeca << " de dano" << endl;
+				Vida = Vida-cabeca;
+				cout << "Vida atual: " << Vida << endl;
+				
+			}
+			if(r==2){
+				int Torax;
+				Torax = rand() % 35 + 1;
+				cout << "Acertou o torax: " << Torax << " de dano" << endl;
+				Vida = Vida-Torax;
+				cout << "Vida atual: " << Vida << endl;
+				
+			}
+			if(r==3){
+				int Membros;
+				Membros = rand() % 25 + 1;
+				cout << "Acertou um membro: " << Membros << " de dano" << endl;
+				Vida = Vida-Membros;
+				cout << "Vida atual: " << Vida << endl;
+			}
+			if(r==4){
+				cout << "Erro" << endl;
+				cout << "Vida atual: " << Vida << endl;
+				
+			}
+		}
+		int combate(){//O ataque do Bandido
+					string i;
+                    cout << "Coloque qualquer coisa para atacar"<< endl;
+                    cin >> i;
+					Dano();
+					cout << "Vida atual do Policial: " << Vida << endl;
+					if(Vida<= 0){
+						return 1;
+					}
+			}
+		
+		void setVida(int Vida_Policia){//Aqui sra guardado a vida do Policial
+			Vida=Vida_Policia;
+		}
+		int getVida(){
+			return Vida;
+		}
+		
+	
+	
+};
